cd990a2a11bf7a344ebb34ab3af8daf9913e10a6
kushner dmitriy <sung1337@yandex.ru>
2018-12-06 02:07:55 +0300

    moved url to local variables;made show method shorter

--- app/controllers/api/v1/tumbs_controller.rb
+++ app/controllers/api/v1/tumbs_controller.rb
@@ -4,29 +4,38 @@ require 'net/http'
 
 class Api::V1::TumbsController < ApplicationController
   before_action :parse_params, only: [:show]
+  WHITE_SCREENSHOT_NAME = 'white'
   # GET tumbs/
   def show
     md = Digest::MD5.hexdigest(@url)
     screenshot = Tumb.find_by(screen_url: md)
     if screenshot
-      send_file "#{Rails.root}/public/screenshots/#{md}.png", type: 'image/png', disposition: 'inline'
+      send_file_to_front(md)
     elsif !screenshot && remote_file_exist?
-        screenshot = Tumb.create(screen_url: md)
-        File.open("#{Rails.root}/public/screenshots/#{md}.png", 'wb') do |fo|
-          fo.write open("http://127.0.0.1:3030/?url=#{@url}", read_timeout: 60).read
-        end
-        send_file "#{Rails.root}/public/screenshots/#{md}.png", type: 'image/png', disposition: 'inline'
+      Tumb.create(screen_url: md)
+      make_file(md)
+      send_file_to_front(md)
     else
-        send_file "#{Rails.root}/public/screenshots/white.png", type: 'image/png', disposition: 'inline'
+      send_file_to_front(WHITE_SCREENSHOT_NAME)
     end
   end
 
   private
 
+  def send_file_to_front(file_name)
+    send_file "#{Rails.root}/public/screenshots/#{file_name}.png", type: 'image/png', disposition: 'inline'
+  end
+
+  def make_file(file_name)
+    File.open("#{Rails.root}/public/screenshots/#{file_name}.png", 'wb') do |fo|
+      fo.write open("#{ENV['SCREENSHOT_API']}/?url=#{@url}").read
+    end
+  end
+
   def remote_file_exist?
-    nurl = URI.parse("http://127.0.0.1:3030/?url=#{@url}")
+    nurl = URI.parse("#{ENV['SCREENSHOT_API']}/?url=#{@url}")
     Net::HTTP.start(nurl.host, nurl.port) do |http|
-      return http.head(nurl.request_uri).code == "200"
+      return http.head(nurl.request_uri).code == '200'
     end
   end
 

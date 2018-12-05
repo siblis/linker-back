f3329f3c0eeea55c31a1ac49e18628b750936c08
deploy <deploy@example.org>
2018-12-05 21:21:44 +0300

    made tumb controller faster

--- app/controllers/api/v1/tumbs_controller.rb
+++ app/controllers/api/v1/tumbs_controller.rb
@@ -6,32 +6,31 @@ class Api::V1::TumbsController < ApplicationController
   before_action :parse_params, only: [:show]
   # GET tumbs/
   def show
-    if remote_file_exist?
-      md = Digest::MD5.hexdigest(@url)
-      screenshot = Tumb.find_by(screen_url: md)
-      unless screenshot
-        screenshot = Tumb.create(screen_url: md) 
+    md = Digest::MD5.hexdigest(@url)
+    screenshot = Tumb.find_by(screen_url: md)
+    if screenshot
+      send_file "#{Rails.root}/public/screenshots/#{md}.png", type: 'image/png', disposition: 'inline'
+    elsif !screenshot && remote_file_exist?
+        screenshot = Tumb.create(screen_url: md)
         File.open("#{Rails.root}/public/screenshots/#{md}.png", 'wb') do |fo|
           fo.write open("http://127.0.0.1:3030/?url=#{@url}", read_timeout: 60).read
         end
-      end
-      send_file "#{Rails.root}/public/screenshots/#{md}.png", type:'image/png', disposition: 'inline'
+        send_file "#{Rails.root}/public/screenshots/#{md}.png", type: 'image/png', disposition: 'inline'
     else
-      send_file "#{Rails.root}/public/screenshots/white.png", type:'image/png', disposition: 'inline'
+        send_file "#{Rails.root}/public/screenshots/white.png", type: 'image/png', disposition: 'inline'
     end
   end
 
   private
- 
+
   def remote_file_exist?
     nurl = URI.parse("http://127.0.0.1:3030/?url=#{@url}")
     Net::HTTP.start(nurl.host, nurl.port) do |http|
       return http.head(nurl.request_uri).code == "200"
     end
   end
-  
+
   def parse_params
     @url = params[:url]
-  end  
-  
+  end
 end
